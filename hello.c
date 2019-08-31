f2d17887d7090334ae1a2f42f6db25753254aa41
savansurep <54682745+savansurep@users.noreply.github.com>
2019-08-31 16:17:55 -0600

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: savan sureshkumar patel
+ * collaborator: Majid Al Amri
+ * Date: 2019/27/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("savan sureshkumar patel, computer science\n");  //printing name and major
+
+  return 0;
+}
